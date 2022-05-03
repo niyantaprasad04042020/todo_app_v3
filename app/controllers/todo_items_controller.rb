0014615288abf8270d7e85003db3574ae4b40d52
NiyantaPrasad <niyanta@zwayam.com>
2022-05-04 02:19:28 +0530

    mark as complete feature done

--- app/controllers/todo_items_controller.rb
+++ app/controllers/todo_items_controller.rb
@@ -1,25 +1,33 @@
 class TodoItemsController < ApplicationController
   before_action :set_todo_list
-
+  before_action :send_todo_item, except: [:create]
   def create
     @todo_item = @todo_list.todo_items.create(params[:todo_item].permit(:content))
     redirect_to @todo_list
   end
 
   def destory
-    @todo_item = @todo_list.todo_items.find(params[:id])
     if @todo_item.destory
       flash[:success] = "Todo list item was deleted"
     else
       flash[:error] = "Todo list item could not be deleted"
     end
     redirect_to @todo_list 
+  end 
+  
+  def complete
+    @todo_item.update_attribute[:completed_at, Time.now]
+    redirect_to @todo_list, notice: "Todo item completed"
   end
 
   private
 
   def set_todo_list
     @todo_list = TodoList.find(params[:todo_list_id])
+  end
+
+  def set_todo_item
+    @todo_item = @todo_list.todo_items.find(params[:id])
   end
 
   def todo_item_params
--- app/models/todo_item.rb
+++ app/models/todo_item.rb
@@ -1,5 +1,10 @@
 class TodoItem
   include Mongoid::Document
   field :content, type: String
+  field :completed_at, tytpe: DateTime
   belongs_to :todo_list
+
+  def completed?
+  	!completed_at.blank?
+  end
 end
